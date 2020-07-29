f62d8e313cee2fe7c351e23fcd02d3a9d66df32b
Riza Ramadan <riza.ramadan@gmail.com>
2020-07-29 08:43:57 +0700

    add new concepts: route

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/rizaramadan/go101
 
 go 1.14
 
-require (
-	github.com/pkg/errors v0.9.1 // indirect
-)
+require github.com/pkg/errors v0.9.1 // indirect
--- httpapi/http_server.go
+++ httpapi/http_server.go
@@ -5,11 +5,12 @@ import (
 	"html"
 	"log"
 	"net/http"
-	"github.com/rizaramadan/go101/httpapi/routes"
+
+	"github.com/rizaramadan/go101/web"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w,"this is home, %q \n",  html.EscapeString(r.URL.Path))
+	_, err := fmt.Fprintf(w, "this is home, %q \n", html.EscapeString(r.URL.Path))
 	if err != nil {
 		log.Println(err)
 	}
@@ -17,14 +18,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w,"hello, %q \n",  html.EscapeString(r.URL.Path))
+		_, err := fmt.Fprintf(w, "hello, %q \n", html.EscapeString(r.URL.Path))
 		if err != nil {
 			log.Println(err)
 		}
 	})
 
-	http.HandleFunc("/home", home)
+	web.NewRoute("/home", home)
 
 	log.Println("listen localhost 8989")
-	log.Fatal(http.ListenAndServe(":8989",nil))
+	log.Fatal(http.ListenAndServe(":8989", nil))
 }
